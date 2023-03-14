5ad0ce1c3bf98908ab1b3983b299874830a4286c
pinakpani-mukherjee <mukherjee.pinakpani14@gmail.com>
2023-03-15 00:31:33 +0900

    printed hello world in boot

--- src/main.asm
+++ src/main.asm
@@ -1,13 +1,61 @@
 org 0x7C00
 bits 16
 
+%define ENDL 0x0D,0x0A
+
+;added jump to main
+start:
+    jmp main
+
+;
+; Prints a string to a screen.
+; Params:
+; -ds:si points to string
+;
+puts:
+    ; save registers to stack that we will modify
+    push si
+    push ax
+
+.loop:
+    lodsb               ;loads next character in al
+    or al, al           ;verify if next character is null?
+    jz .done
+
+    mov ah, 0x0e
+    mov bh, 0
+    int 0x10
+
+
+    jmp .loop
+
+.done:
+    pop ax
+    pop si
+    ret 
+
 
 main:
+    ; setup data segments
+    mov ax, 0
+    mov ds, ax
+    mov es, ax 
+
+    ; setup stack
+    mov ss, ax
+    mov sp, 0x7c00 ;stack grows downwards
+    
+    ;print message 
+    mov si, msg_hello
+    call puts
+
     hlt
 
 .halt:
     jmp .halt
 
+
+msg_hello: db 'Hello World', ENDL, 0
 
 times 510-($-$$) db 0
 dw 0AA55h
